344e8c6d21a2c1bbbb58c8ff12e3db79782f6f27
josemaldonadojr <jose@josemaldonado.dev>
2023-02-08 16:11:33 -0600

    refactor

--- first_non_repeated_character.js
+++ first_non_repeated_character.js
@@ -2,10 +2,25 @@
 
 const inputString = "cbcbdde";
 
-const characterArray = Array.from(inputString);
+// const characterArray = Array.from(inputString);
 
-const firstNonRepeatedCharacter = characterArray.find(
-  (char) => characterArray.indexOf(char) === characterArray.lastIndexOf(char)
-);
+// const firstNonRepeatedCharacter = characterArray.find(
+//   (char) => characterArray.indexOf(char) === characterArray.lastIndexOf(char)
+// );
+
+// console.log(firstNonRepeatedCharacter);
+
+const uniqueCharacterSet = new Set();
+
+for (const character of inputString) {
+  if (uniqueCharacterSet.has(character)) {
+    uniqueCharacterSet.delete(character);
+  } else {
+    uniqueCharacterSet.add(character);
+  }
+}
+
+const uniqueCharacterSetIterator = uniqueCharacterSet.values();
+const firstNonRepeatedCharacter = uniqueCharacterSetIterator.next().value;
 
 console.log(firstNonRepeatedCharacter);
